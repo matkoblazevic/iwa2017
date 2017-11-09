85b298390980315b8aa6aac72f36053ec6be63bc
Matko Blazevic <matko.b@yahoo.com>
2017-11-09 18:15:42 +0100

    added calc

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 console.log('Radi');
 let deleteMsg = 'Are you sure you wanna delete?'
 console.log(deleteMsg);
+const calc = (num1, num2) => {
+  return num1 * num2;
+};
+console.log(calc(45, 64));
